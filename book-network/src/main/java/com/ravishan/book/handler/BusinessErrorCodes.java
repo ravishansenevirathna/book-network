5d2e98e08d8789e5a136fea9a9ebc3e3aa8ce8bb
ravishansenevirathna <travishan55@gmail.com>
2024-06-16 11:20:57 +0530

    6th commit

---
+++ book-network/src/main/java/com/ravishan/book/handler/BusinessErrorCodes.java
@@ -0,0 +1,33 @@
+package com.ravishan.book.handler;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+/**
+ * Hi 👋, I'm ravishansenevirathna
+ * Project : Book Social Network
+ * Created date : 6/16/2024
+ * Created time : 11:03 AM
+ */
+public enum BusinessErrorCodes {
+    NO_CODE(0, HttpStatus.NOT_IMPLEMENTED, "No code"),
+    INCORRECT_CURRENT_PASSWORD(300, HttpStatus.BAD_REQUEST, "Current password is incorrect"),
+    NEW_PASSWORD_DOES_NOT_MATCH(301, HttpStatus.BAD_REQUEST, "The new password does not match"),
+    ACCOUNT_LOCKED(302, HttpStatus.FORBIDDEN, "User account is locked"),
+    ACCOUNT_DISABLED(303, HttpStatus.FORBIDDEN, "User account is disabled"),
+    BAD_CREDENTIALS(304, HttpStatus.FORBIDDEN, "Login and / or Password is incorrect"),
+    ;
+
+    @Getter
+    private final int code;
+    @Getter
+    private final String description;
+    @Getter
+    private final HttpStatus httpStatus;
+
+    BusinessErrorCodes(int code, HttpStatus status, String description) {
+        this.code = code;
+        this.description = description;
+        this.httpStatus = status;
+    }
+}
